e09753b73d722012936a40dbdaed1bf3213f609f
Sergey Bekzin <vip.bekezin@mail.ru>
2024-01-31 21:49:46 +0300

    Initial Commit

---
+++ TaGOnrog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaGOnrog
+//
+//  Created by Сергей Бекезин on 31.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
